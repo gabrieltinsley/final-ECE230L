a7ba3e879cc94af9607efd8870d063d1889acc7a
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-30 12:48:41 -0600

    Update first_multi.v

--- first_multi.v
+++ first_multi.v
@@ -21,4 +21,29 @@ module first_multi(
   input enable,
   output reg [7:0] data
 );
+
+  always @(*) begin
+    if (enable) begin
+      case (sel)
+        4'b0000: data = zero;
+        4'b0001: data = one;
+        4'b0010: data = two;
+        4'b0011: data = three;
+        4'b0100: data = four;
+        4'b0101: data = five;
+        4'b0110: data = six;
+        4'b0111: data = seven;
+        4'b1000: data = eight;
+        4'b1001: data = nine;
+        4'b1010: data = A_ten;
+        4'b1011: data = B_eleven;
+        4'b1100: data = C_twelve;
+        4'b1101: data = D_thirteen;
+        4'b1110: data = E_fourteen;
+        4'b1111: data = F_fifteen;
+        end else begin
+          data = 0;
+        end 
+      end
+endmodule
   
