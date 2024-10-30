a328dfcaa3ba79c2d6c378234f55853e0e021566
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-10-30 11:22:52 -0600

    Update logic.v

--- logic.v
+++ logic.v
@@ -26,6 +26,10 @@ module logic
 );
   genvar i;
   wire [16:0] bits_out;
+
+  // each iteration through the for loop instiates and AND's the number A and B together 
+
+  // possibly make a decoder to choose when to do AND / OR logic
   
   generate
     for(i = 0; i < BITS; i = i + 1) begin 
