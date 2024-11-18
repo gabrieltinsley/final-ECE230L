89cec42f4b48a033128a3067ca33474563a631d7
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-11-18 11:51:25 -0700

    Update mux.v

--- mux.v
+++ mux.v
@@ -9,40 +9,40 @@ module mux(
   input [7:0] AND,
   input [7:0] OR,
   input [7:0] XOR,
-  input [7:0] XNOR,
   input [7:0] NAND,
-  input [7:0] A_ten,
-  input [7:0] B_eleven,
-  input [7:0] C_twelve,
-  input [7:0] D_thirteen,
-  input [7:0] E_fourteen,
-  input [7:0] F_fifteen,
-  input [3:0] sel,
+  input [7:0] NOR,
+  input [7:0] XNOR,
+  input [7:0] NOT,
+  input [7:0] INV,
+  input [7:0] NEG,
+  input [7:0] STO,
+  input [7:0] SWP,
+  input [3:0] LOAD,
   input enable,
-  output reg [7:0] data
+  output reg [7:0] Y
 );
 
   always @(*) begin
     if (enable) begin
       case (sel)
-        4'b0000: data = zero;
-        4'b0001: data = one;
-        4'b0010: data = two;
-        4'b0011: data = three;
-        4'b0100: data = four;
-        4'b0101: data = five;
-        4'b0110: data = six;
-        4'b0111: data = seven;
-        4'b1000: data = eight;
-        4'b1001: data = nine;
-        4'b1010: data = A_ten;
-        4'b1011: data = B_eleven;
-        4'b1100: data = C_twelve;
-        4'b1101: data = D_thirteen;
-        4'b1110: data = E_fourteen;
-        4'b1111: data = F_fifteen;
+        4'b0000: Y = ADD;
+        4'b0001: Y = SUB;
+        4'b0010: Y = SHL;
+        4'b0011: Y = SHR;
+        4'b0100: Y = CMP;
+        4'b0101: Y = AND;
+        4'b0110: Y = OR;
+        4'b0111: Y = XOR;
+        4'b1000: Y = NAND;
+        4'b1001: Y = NOR;
+        4'b1010: Y = XNOR;
+        4'b1011: Y = NOT;
+        4'b1100: Y = INV;
+        4'b1101: Y = NEG;
+        4'b1110: Y = STO;
+        4'b1111: Y = SWAP;
         end else begin
-          data = 0;
+          Y = LOAD;
         end 
       end
 endmodule
