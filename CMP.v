da9aa65ae2d6bf704ab1b6e0496b4f977a631c32
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-11-18 08:14:28 -0700

    Create CMP.v

---
+++ CMP.v
@@ -0,0 +1,15 @@
+module CMP(
+  input [7:0] A,
+  input [7:0] B,
+  output reg [7:0] Y
+);
+
+  always @(A, B) begin
+    if(A == B)
+      Y <= 0;
+    else if(A > B)
+      Y <= 1;
+    else 
+      Y <= -1;
+  end
+endmodule
