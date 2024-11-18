a7bea47195c11a0f16a23941e8892a61f7356402
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-11-18 13:56:40 -0700

    Update top.v

--- top.v
+++ top.v
@@ -36,8 +36,80 @@ module top
     // Split switches into two 4-bit signals A and B
     wire [3:0] lowerY;
     wire [3:0] upperY;
-    
-   
+
+    // Intermediate wires
+    wire [7:0] data_from_mux_A;
+    wire [7:0] data_from_mux_B;
+    wire [7:0] data_from_mux_Y;
+
+    // Instantiate mux_A
+    mux mux_A (
+        .ADD(ADD_A), 
+        .SUB(SUB_A), 
+        .SHL(SHL_A), 
+        .SHR(SHR_A),
+        .CMP(CMP_A), 
+        .AND(AND_A), 
+        .OR(OR_A), 
+        .XOR(XOR_A),
+        .NAND(NAND_A), 
+        .NOR(NOR_A), 
+        .XNOR(XNOR_A), 
+        .NOT(NOT_A),
+        .INV(INV_A), 
+        .NEG(NEG_A), 
+        .STO(STO_A), 
+        .SWP(SWP_A),
+        .LOAD(LOAD_A), 
+        .sel(sel_A), 
+        .data(data_from_mux_A)
+    );
+
+    // Instantiate mux_B
+    mux mux_B (
+        .ADD(ADD_B), 
+        .SUB(SUB_B), 
+        .SHL(SHL_B), 
+        .SHR(SHR_B),
+        .CMP(CMP_B), 
+        .AND(AND_B), 
+        .OR(OR_B), 
+        .XOR(XOR_B),
+        .NAND(NAND_B), 
+        .NOR(NOR_B), 
+        .XNOR(XNOR_B), 
+        .NOT(NOT_B),
+        .INV(INV_B), 
+        .NEG(NEG_B), 
+        .STO(STO_B), 
+        .SWP(SWP_B),
+        .LOAD(LOAD_B), 
+        .sel(sel_B), 
+        .data(data_from_mux_B)
+    );
+
+    // Instantiate mux_Y
+    mux mux_Y (
+        .ADD(ADD_Y), 
+        .SUB(SUB_Y), 
+        .SHL(SHL_Y), 
+        .SHR(SHR_Y),
+        .CMP(CMP_Y), 
+        .AND(AND_Y), 
+        .OR(OR_Y), 
+        .XOR(XOR_Y),
+        .NAND(NAND_Y), 
+        .NOR(NOR_Y), 
+        .XNOR(XNOR_Y), 
+        .NOT(NOT_Y),
+        .INV(INV_Y), 
+        .NEG(NEG_Y), 
+        .STO(STO_Y), 
+        .SWP(SWP_Y),
+        .LOAD(LOAD_Y), 
+        .sel(sel_Y), 
+        .data(data_from_mux_Y)
+    );
 
 
     // Instantiate the 7-segment scanner (to cycle through the anodes)
