bc134dbde71eee8feabf4592a7270e6e75e06d81
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-11-18 14:04:17 -0700

    Update seven_seg_decoder.v

--- seven_seg_decoder.v
+++ seven_seg_decoder.v
@@ -1,6 +1,5 @@
 module seven_seg_decoder(
     input [7:0] Val,
-    input [7:0] Val_2,
     input [3:0] lowerY,
     input [3:0] upperY,
     input [3:0] anode,
@@ -15,7 +14,7 @@ module seven_seg_decoder(
     always @(*) begin
       case (anode)
          'b1110: selected_sig <= Val; // Right
-         'b1101: selected_sig <= Val_2; // Right Center
+         'b1101: selected_sig <= 0; // Right Center
          'b1011: selected_sig <= lowerY; // Left Center
          'b0111: selected_sig <= upperY; // Left
       endcase
