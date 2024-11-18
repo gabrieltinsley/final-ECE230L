c8be8b882e2150265b23bf0657e15f8aa3969ca3
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-11-18 12:21:38 -0700

    Update clock_div.v

--- clock_div.v
+++ clock_div.v
@@ -1,20 +1,3 @@
-module dff(
-    input reset,
-    input clock,
-    input D,
-    output reg Q,
-    output NotQ
-);
-
-    always @(posedge reset, posedge clock) begin
-        if (reset) begin
-            Q <= 0;
-        end else if (clock) begin
-            Q <= D;
-        end
-    end
-     assign NotQ = ~Q;
-endmodule
 
 module dff(
     input reset,
