f6d33c41c2024bbe48e4ce8b8efd240ebc34ad9d
gabrieltinsley <156216375+gabrieltinsley@users.noreply.github.com>
2024-11-19 07:50:30 -0700

    Update mux.v

--- mux.v
+++ mux.v
@@ -24,26 +24,26 @@ module mux (
     always @(*) begin
         if (enable) begin
             case (sel)
-                4'b0000: data => ADD;
-                4'b0001: data => SUB;
-                4'b0010: data => SHL;
-                4'b0011: data => SHR;
-                4'b0100: data => CMP;
-                4'b0101: data => AND;
-                4'b0110: data => OR;
-                4'b0111: data => XOR;
-                4'b1000: data => NAND;
-                4'b1001: data => NOR;
-                4'b1010: data => XNOR;
-                4'b1011: data => NOT;
-                4'b1100: data => INV;
-                4'b1101: data => NEG;
-                4'b1110: data => STO;
-                4'b1111: data => SWP;
+                4'b0000: data <= ADD;
+                4'b0001: data <= SUB;
+                4'b0010: data <= SHL;
+                4'b0011: data <= SHR;
+                4'b0100: data <= CMP;
+                4'b0101: data <= AND;
+                4'b0110: data <= OR;
+                4'b0111: data <= XOR;
+                4'b1000: data <= NAND;
+                4'b1001: data <= NOR;
+                4'b1010: data <= XNOR;
+                4'b1011: data <= NOT;
+                4'b1100: data <= INV;
+                4'b1101: data <= NEG;
+                4'b1110: data <= STO;
+                4'b1111: data <= SWP;
             endcase
         end else begin
             // When enable is low, output LOAD value expanded to 8 bits
-            data => LOAD; // Concatenate 4'b0000 with 4-bit LOAD to make 8 bits
+            data <= LOAD; // Concatenate 4'b0000 with 4-bit LOAD to make 8 bits
         end
     end
 endmodule
